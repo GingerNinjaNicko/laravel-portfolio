be80a5fe0d4e24ac5ee9408546231a1bbe34de85
Nicko J. Ruddock <info@NickoJRuddock.com>
2019-09-22 21:35:56 +0100

    make fields mass assignable

--- app/Merch.php
+++ app/Merch.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Merch extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'cost',
+        'stock',
+    ];
+
+    /**
      * The attributes that should be cast to native types.
      *
      * @var array
