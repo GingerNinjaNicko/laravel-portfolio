e59cc6858000be9ca1528809baa09dd530058be8
Nicko J. Ruddock <info@NickoJRuddock.com>
2019-09-23 09:53:27 +0100

    refactor message display

--- resources/views/examples/merch/index.blade.php
+++ resources/views/examples/merch/index.blade.php
@@ -5,13 +5,6 @@
 @endsection
 
 @section('content')
-    @if (session('message'))
-        <div class="container">
-            <div class="alert alert-{{ session('status') ?? 'success' }}" role="alert">
-                {{ session('message') }}
-            </div>
-        </div>
-    @endif
     <div class="container">
         <div class="jumbotron row">
             <div class="col-sm-12 text-center">
