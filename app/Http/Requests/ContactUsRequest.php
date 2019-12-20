f3e99c095621b9f7e8f32e0f21b5542c8459a504
Nicko Ruddock <nickojruddock@gmail.com>
2019-12-20 15:45:30 +0000

    Add emailConfirm honeypot validation

--- app/Http/Requests/ContactUsRequest.php
+++ app/Http/Requests/ContactUsRequest.php
@@ -28,6 +28,7 @@ class ContactUsRequest extends FormRequest
         return [
             'name' => 'required|string',
             'email' => 'required|email',
+            'emailConfirm' => 'size:0'
             'message' => 'required|string',
             'robot' => 'required|in:no,No,Nope,nope',
         ];
@@ -44,5 +45,24 @@ class ContactUsRequest extends FormRequest
             'robot.required' => 'Please confirm you are not a robot',
             'robot.in' => 'Please confirm you are not a robot',
         ];
+    }
+
+    /**
+     * Overwrite base - Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        if ($validator->errors()->get('emailTwo')) {
+            $this->redirectRoute = 'thanks';
+        }
+
+        throw (new ValidationException($validator))
+                    ->errorBag($this->errorBag)
+                    ->redirectTo($this->getRedirectUrl());
     }
 }
