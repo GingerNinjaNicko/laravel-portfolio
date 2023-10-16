0a3d459e8bdedcd2933b1eb352003bca9c7922fd
Nick Ruddock <Nick.Ruddock@taxassist.co.uk>
2023-10-16 17:21:26 +0100

    import controllers in route file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MerchController;
+use App\Http\Controllers\ContactUsController;
 
 /*
 |--------------------------------------------------------------------------
